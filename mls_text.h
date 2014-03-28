c22e7fd8f3119e415f61c0bebf8301d948d4e46e
Jens John <jens.o.john@gmail.com>
2014-03-28 14:22:47 +0900

    fix array type error in mls_text.h

--- mls_text.h
+++ mls_text.h
@@ -30,7 +30,7 @@ void PrintTop(nTptr, int);
 void PrintTopQ(nTptr, int);
 void PrintTopHtml(nTptr, int);
 void PrintGraph(long[], int, int);
-void PrintGraphHtml(long[], long[][], int, char *);
+void PrintGraphHtml(long[], long **, int, char *);
 
 void PrintStatText(int, char *);
 void PrintStatHtml(int, char *, short);
